28427365477e2db4459d31417a44dd3d6cef024a
omar214 <omarmohamed2322000@gmail.com>
2022-08-15 17:45:25 +0200

    add tests

---
+++ src/__test__/index.spec.ts
@@ -0,0 +1,55 @@
+import supertest, { Response } from 'supertest';
+import app from '../index';
+
+const request = supertest(app);
+
+describe('test sending bad requests', () => {
+  it('sending bad request get status code 400', async () => {
+    const response: Response = await request.get(
+      '/api/image?&width=100&height=100'
+    );
+
+    expect(response.status).toBe(400);
+  });
+
+  it('sending image that is not found get code 404', async () => {
+    const response: Response = await request.get(
+      '/api/image?imageName=wrongname&width=100&height=100'
+    );
+
+    expect(response.status).toBe(404);
+  });
+
+  it('sending width & height as strings get code 400', async () => {
+    const response: Response = await request.get(
+      '/api/image?&width=100&height=100'
+    );
+    expect(response.status).toBe(400);
+  });
+
+  it('sending -ve dimensions get code 400', async () => {
+    const response: Response = await request.get(
+      '/api/image?&width=100&height=100'
+    );
+
+    expect(response.status).toBe(400);
+  });
+});
+
+describe('sending good requests', () => {
+  it('test resize image get code 200', async () => {
+    const response: Response = await request.get(
+      '/api/image?imageName=encenadaport&width=100&height=100'
+    );
+
+    expect(response.status).toBe(200);
+  });
+
+  it('test cached image get code 200', async () => {
+    const response: Response = await request.get(
+      '/api/image?imageName=encenadaport&width=100&height=100'
+    );
+
+    expect(response.status).toBe(200);
+  });
+});
