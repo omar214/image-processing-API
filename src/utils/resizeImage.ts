22fd089bd797bed903bff65d37c2522f9d117eea
omar214 <omarmohamed2322000@gmail.com>
2022-08-15 05:06:16 +0200

    add caching logic

--- src/utils/resizeImage.ts
+++ src/utils/resizeImage.ts
@@ -1,14 +1,30 @@
+import { existsSync } from 'fs';
+import path from 'path';
 import sharp from 'sharp';
 
 const resize = async (imageName: string, width: number, height: number) => {
   try {
-    const currentImagePath = `${__dirname}/../images/${imageName}.jpg`;
     const newImageName = `${imageName}_${width}_${height}.jpg`;
-    const newImagePath = `${__dirname}/../images/cached/${newImageName}`;
+    const currentImagePath = path.join(
+      __dirname,
+      '..',
+      '..',
+      'public',
+      'images',
+      `${imageName}.jpg`
+    );
+    const newImagePath = path.join(
+      __dirname,
+      '..',
+      '..',
+      'public',
+      'cached',
+      newImageName
+    );
+    // handle cached images
+    if (existsSync(newImagePath)) return newImagePath;
 
-    const result = await sharp(currentImagePath)
-      .resize(width, height)
-      .toFile(`${newImagePath}`);
+    await sharp(currentImagePath).resize(width, height).toFile(newImagePath);
     return newImagePath;
   } catch (error) {
     console.log(error);
